82c792397cadda3637354343d49dd92423b002d6
aug-vishnu <vishnuprabhu.bvk@gmail.com>
2021-04-25 13:05:30 +0530

    Removed links and animations

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,3 @@
-$(window).scroll(function() {
-    console.log("ashduh");
-    if ($(window).scrollTop() > 10) {
-        $('#navBar').addClass('floatingNav');
-    } else {
-        $('#navBar').removeClass('floatingNav');
-    }
-});
-
 // Fun for contact page
 $("form").submit(function() {
     // alert("Submitted");
@@ -25,19 +16,24 @@ $(document).ready(function() {
 
     /* Every time the window is scrolled ... */
     $(window).scroll(function() {
-
+        if ($(window).scrollTop() > 10) {
+            $('#navBar').addClass('floatingNav');
+        } else {
+            $('#navBar').removeClass('floatingNav');
+        }
         /* Check the location of each desired element */
         $('.vent').each(function(i) {
 
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
+            console.log(bottom_of_object, bottom_of_window);
 
             /* If the object is completely visible in the window, fade it it */
             if (bottom_of_window > bottom_of_object) {
 
                 $(this).animate({
                     'opacity': '1'
-                }, 1500);
+                }, 1200);
 
             }
 
